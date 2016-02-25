56f25c40570d50ff57b4bdd0d460fad58a50d639
FArtem <lolpikds@mail.ru>
2016-02-25 19:45:13 +0300

    Update log.js
    engine.myy(id)

--- game/game/file/js/en/log.js
+++ game/game/file/js/en/log.js
@@ -63,19 +63,20 @@ $(window).swipe({
 		else if (phase == 'end') {}
 	},
 	swipeLeft:function(event, direction, distance, duration, fingerCount) {
-		engine.myy(2);
+		engine.myy(2);// dir engine.js > move char to left
 	},
 	swipeRight:function(event, direction, distance, duration, fingerCount) {
-		engine.myy(1);
+		engine.myy(1);// dir engine.js > move char to right
 	},
 	swipeUp:function(event, direction, distance, duration, fingerCount) {
-		engine.myy(3);
+		engine.myy(3);// dir engine.js > move char to top
 	},
 	swipeDown:function(event, direction, distance, duration, fingerCount) {
-		engine.myy(4);
+		engine.myy(4);// dir engine.js > move char to bot
 	},
 	threshold: 30,
 	triggerOnTouchEnd: false,
 		allowPageScroll: "vertical",
 		excludedElements: "button, input, select, textarea, .noSwipe"
-});
+});//engine.myy(id) - can be used anywhere where adjacent connected script of the game\
+//можно использовать везде, где указан скрипт игры, можно вынести и свои функции на повтор.
